b90bd5f4cc21d04ba00ab6c2725ef343af92d7b5
Rakib <rakib@vida.com.bd>
2024-04-05 12:31:18 +0600

    first commit

---
+++ asset/js/script.js
@@ -0,0 +1,15 @@
+// $(function () {
+
+//     'use strict';
+  
+//     $('.ad').slick({
+//         infinite: true,
+//         slidesToShow: 1,
+//         autoplay: true,
+//         slidesToScroll: 1,
+//         arrows: false,
+//         dots: false,
+//     });
+
+// });
+
